62968d19b6a3e612222fdc9983893eff80f79af4
Woomin <worejj@gmail.com>
2021-03-01 19:07:13 +0900

    2615

--- 2615.cpp
+++ 2615.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+int map[21][21];
 
 int search1(int first, int i, int j) {
 	int cnt = 1;
@@ -16,7 +17,13 @@ int search3(int first, int i, int j) {
 	for (; first == map[i - 1][j + 1]; i--, j++) cnt++;
 	return cnt == 5 ? 1 : 0;
 }
-int map[21][21];
+int search4(int first, int i, int j) {
+	int cnt = 1;
+	for (; first == map[i + 1][j + 1]; i++,j++) cnt++;
+	return cnt == 5 ? 1 : 0;
+}
+
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -29,9 +36,29 @@ int main() {
 
 	for (int i = 1; i < 20; i++) {
 		for (int j = 1; j < 20; j++) {
-			if((map[i][j] != map[i][j-1]) && )
+			if (map[i][j] != 0) {
+				if ((map[i][j] != map[i][j - 1]) && (search1(map[i][j], i, j) == 1)) {
+					cout << map[i][j] << '\n' << i << ' ' << j;
+					return 0;
+				}
+				if ((map[i][j] != map[i - 1][j]) && (search2(map[i][j], i, j) == 1)) {
+					cout << map[i][j] << '\n' << i << ' ' << j;
+					return 0;
+				}
+				if ((map[i][j] != map[i + 1][j - 1]) && (search3(map[i][j], i, j) == 1)) {
+					cout << map[i][j] << '\n' << i << ' ' << j;
+					return 0;
+				}
+				if ((map[i][j] != map[i - 1][j - 1]) && (search4(map[i][j], i, j) == 1)) {
+					cout << map[i][j] << '\n' << i << ' ' << j;
+					return 0;
+				}
+			}
 		}
 	}
+	cout << "0\n";
+
+	return 0;
 
 	
 }
